60025b77301b4485268fe3a44f99da9ea0e21d31
David Israel Martinez Hernandez <david.14yeah@gmail.com>
2023-04-02 21:31:47 -0600

    CREATE: Iniciar proyecto Net 6

---
+++ PlatziWebApi/Middlewares/TimeMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace PlatziWebApi.Middlewares
+{
+    public class TimeMiddleware
+    {
+        //Nos ayuda a llamar al siguiente middleware que sigue
+        readonly RequestDelegate next;
+
+        public TimeMiddleware(RequestDelegate nextRequest) 
+        {
+            next = nextRequest;   
+        }
+
+        public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext context)
+        {
+            await next(context);
+
+            if (context.Request.Query.Any(p => p.Key == "time"))
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToShortDateString());
+            }
+        }
+      
+    }
+
+    public static class TimeMiddlewareExtension
+    {
+        public static IApplicationBuilder UserTimeMiddleware(this IApplicationBuilder build)
+        {
+            return build.UseMiddleware<TimeMiddleware>();
+        }
+    }
+}
---
+++ PlatziWebApi/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Diagnostics;
+using PlatziWebApi.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//Muestra una vista en la raiz de la API
+app.UseWelcomePage();
+
+app.UserTimeMiddleware();
+
+app.MapControllers();
+
+app.Run();
